ba840ce88d97743f3e99da98e114cc982bc39d10
frank <franktowa199724@gmail.com>
2023-12-08 13:21:22 +0100

    Second Commit on sum

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ export const sum = (a, b) => {
 
   return a + b;
 };
+
+export const mysizeAlphaT = (str = '') => {
+  let count = 0;
+
+  if (typeof str !== 'string') {
+    return count;
+  }
+
+  while (!str[count]) {
+    count++;
+  }
+
+  return count;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-import { myAalphaNumberT } from '../src/index';
+import { myAalphaNumberT, sum } from '../src/index';
 
 describe('myAalphaNumberT', () => {
   it('Should return hello when hello is in string', () => {
@@ -15,5 +15,21 @@ describe('myAalphaNumberT', () => {
   });
 
 });
+
+describe('sum', () => {
+  it('Should return the same of the two numbers', () => {
+    expect(sum(2, 3)).toBe(5);
+  });
+  it('Should return O if the argument is not a number', () => {
+    expect(sum('a', 3)).toBe(0);
+  });
+  it('Should return negative number', () => {
+    expect(sum(-2, 3)).toBe(1);
+  });
+
+
+});
+
+
 
 
